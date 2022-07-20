ffe4a84ceae034f0fce05f49f925a1bbaeb64574
David Hasegawa <david.hasegawa@braincorp.com>
2022-07-19 17:45:46 -0700

    Fix indents

--- grub-core/fs/hfsplus.c
+++ grub-core/fs/hfsplus.c
@@ -19,7 +19,7 @@
 
 /* HFS+ is documented at http://developer.apple.com/technotes/tn/tn1150.html */
 
-#define grub_fshelp_node grub_hfsplus_file 
+#define grub_fshelp_node grub_hfsplus_file
 #include <grub/err.h>
 #include <grub/file.h>
 #include <grub/mm.h>
@@ -145,7 +145,7 @@ grub_hfsplus_read_block (grub_fshelp_node_t node, grub_disk_addr_t fileblock)
 {
   struct grub_hfsplus_btnode *nnode = 0;
   grub_disk_addr_t blksleft = fileblock;
-  struct grub_hfsplus_extent *extents = node->compressed 
+  struct grub_hfsplus_extent *extents = node->compressed
     ? &node->resource_extents[0] : &node->extents[0];
 
   while (1)
@@ -461,7 +461,7 @@ grub_hfsplus_cmp_extkey (struct grub_hfsplus_key *keya,
 
   if (extkey_a->type < extkey_b->type)
     return -1;
-  
+
   akey = grub_be_to_cpu32 (extkey_a->start);
   if (akey > extkey_b->start)
     return 1;
@@ -548,7 +548,7 @@ grub_hfsplus_btree_search (struct grub_hfsplus_btree *btree,
 			   struct grub_hfsplus_key_internal *key,
 			   int (*compare_keys) (struct grub_hfsplus_key *keya,
 						struct grub_hfsplus_key_internal *keyb),
-			   struct grub_hfsplus_btnode **matchnode, 
+			   struct grub_hfsplus_btnode **matchnode,
 			   grub_off_t *keyoffset)
 {
   grub_uint64_t currnode;
@@ -725,24 +725,24 @@ list_nodes (void *record, void *hook_arg)
       catkey->name[i] = grub_be_to_cpu16 (catkey->name[i]);
 
       if (catkey->name[i] == '/')
-	catkey->name[i] = ':';
+       catkey->name[i] = ':';
 
       /* If the name is obviously invalid, skip this node.  */
       if (catkey->name[i] == 0)
-	{
-	  grub_free (filename);
-	  return 0;
-	}
+       {
+         grub_free (filename);
+         return 0;
+       }
     }
 
   *grub_utf16_to_utf8 ((grub_uint8_t *) filename, catkey->name,
-		       grub_be_to_cpu16 (catkey->namelen)) = '\0';
+                      grub_be_to_cpu16 (catkey->namelen)) = '\0';
 
   /* Restore the byte order to what it was previously.  */
   for (i = 0; i < grub_be_to_cpu16 (catkey->namelen); i++)
     {
       if (catkey->name[i] == ':')
-	catkey->name[i] = '/';
+       catkey->name[i] = '/';
       catkey->name[i] = grub_be_to_cpu16 (catkey->name[i]);
     }
 
@@ -1009,7 +1009,7 @@ grub_hfsplus_label (grub_device_t device, char **label)
 
       /* If the name is obviously invalid, skip this node.  */
       if (catkey->name[i] == 0)
-	return 0;
+       return 0;
     }
 
   *label = grub_malloc (label_len * GRUB_MAX_UTF8_PER_UTF16 + 1);
@@ -1017,7 +1017,7 @@ grub_hfsplus_label (grub_device_t device, char **label)
     return grub_errno;
 
   *grub_utf16_to_utf8 ((grub_uint8_t *) (*label), catkey->name,
-		       label_len) = '\0';
+                      label_len) = '\0';
 
   grub_free (node);
   grub_free (data);
